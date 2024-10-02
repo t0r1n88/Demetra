426dd0abc58b807423c00044eeb40a8769ef5869
Oleg Budaev <t0r1n88@mail.ru>
2024-10-02 12:48:19 +0800

    Добавил в отчет БРИТ подсчет обучающихся и тех кто в академе

--- create_social_passport.py
+++ create_social_passport.py
@@ -645,6 +645,7 @@ def create_social_report(etalon_file:str,data_folder:str, path_end_folder:str,ch
             counting_report_wb.save(f'{path_end_folder}/Свод по колонкам Подсчета от {current_time}.xlsx')
 
         # Создаем Свод по статусам
+        main_df.replace('','Нет статуса',inplace=True)
         # Собираем колонки содержащие слово Статус_ и Подсчет_
         lst_status = [name_column for name_column in main_df.columns if 'Статус_' in name_column or 'Подсчет_' in name_column]
         # Создаем датафрейм с данными по статусам
