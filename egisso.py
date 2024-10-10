2e9ad9e59e94006611b9d57c6cc671c20d4ae176
Oleg Budaev <t0r1n88@mail.ru>
2024-10-10 12:05:28 +0800

    Реалзиовал сбор отдельных датафреймов

--- egisso.py
+++ egisso.py
@@ -35,6 +35,29 @@ def find_cols_benefits(lst_cols:list):
     return ben_dct
 
 
+def add_cols_pers_data(df:pd.DataFrame,ben_cols:list,req_cols_lst:list,name_col_ben:str):
+    """
+    Функция для добавления колонок с персональными данными
+    :param df: полный датафрейм с данными
+    :param ben_cols: список колонок относящихся к льготе
+    :param req_cols_lst: наименования колонок которые нужно добавить
+    :param name_col_ben: наименование льготы
+    :return: датафрейм с добавленными колонками
+    """
+    df = df[df[name_col_ben].notna()] # убираем незаполненные строки
+    ben_df = df[ben_cols] # начинаем собирать датафрейм льгот
+    ben_df.insert(0,'Льгота',name_col_ben) # добавляем колонку определяющую что за льгота
+
+    for name_column in req_cols_lst:
+        if name_column in df.columns:
+            ben_df[name_column] = df[name_column]
+        else:
+            ben_df[name_column] = f'Не найдена колонка с названием {name_column}'
+    ben_df.insert(10,'Тип документа','')
+    ben_df.to_excel('data/dfsf.xlsx',index=False)
+
+
+
 
 
 
@@ -51,7 +74,13 @@ def create_part_egisso_data(df:pd.DataFrame):
     benefits_cols_dct = find_cols_benefits(lst_cols_df)
 
     # список требуемых колонок для персональных данных
-    req_lst_personal_data_cols = ['СНИЛС','','','','','','','','','','','','','','','',]
+    req_lst_personal_data_cols = ['СНИЛС','Фамилия','Имя','Отчество','Пол','Дата_рождения','Серия_паспорта','Номер_паспорта',
+                                  'Дата_выдачи_паспорта','Кем_выдан']
+
+    # Собираем датафреймы
+    for name_benefit,ben_cols in benefits_cols_dct.items():
+        temp_df_full = add_cols_pers_data(df.copy(),ben_cols,req_lst_personal_data_cols,name_benefit)
+
 
 
 
