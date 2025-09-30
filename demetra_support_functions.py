f59a775db34a1739c3ef24477351a7c9255946c5
Oleg Budaev <t0r1n88@mail.ru>
2025-09-30 08:28:33 +0800

    Добавил прототип функции по извлечению данных из учебных планов для тарификации

--- demetra_support_functions.py
+++ demetra_support_functions.py
@@ -1323,3 +1323,49 @@ def convert_snils_not_dash(snils):
         return out_snils
     else:
         return f'Неправильное значение!В СНИЛС должно быть 11 цифр - {snils} -{len(result)} цифр'
+
+
+
+def write_df_up_to_excel(dct_df: dict, write_index: bool) -> openpyxl.Workbook:
+    """
+    Функция для записи датафрейма в файл Excel
+    :param dct_df: словарь где ключе это название создаваемого листа а значение датафрейм который нужно записать
+    :param write_index: нужно ли записывать индекс датафрейма True or False
+    :return: объект Workbook с записанными датафреймами
+    """
+    wb = openpyxl.Workbook()  # создаем файл
+    count_index = 0  # счетчик индексов создаваемых листов
+    for name_sheet, df in dct_df.items():
+        wb.create_sheet(title=name_sheet, index=count_index)  # создаем лист
+        # записываем данные в лист
+        none_check = None  # чекбокс для проверки наличия пустой первой строки, такое почему то иногда бывает
+        for row in dataframe_to_rows(df, index=write_index, header=True):
+            if len(row) == 1 and not row[0]:  # убираем пустую строку
+                none_check = True
+                wb[name_sheet].append(row)
+            else:
+                wb[name_sheet].append(row)
+        if none_check:
+            wb[name_sheet].delete_rows(2)
+
+        # ширина по содержимому
+        # сохраняем по ширине колонок
+        for column in wb[name_sheet].columns:
+            max_length = 0
+            column_name = get_column_letter(column[0].column)
+            for cell in column:
+                try:
+                    if len(str(cell.value)) > max_length:
+                        max_length = len(cell.value)
+                except:
+                    pass
+            adjusted_width = (max_length + 2)
+            if adjusted_width >= 80:
+                wb[name_sheet].column_dimensions[column_name].width = 80
+                for cell in wb[name_sheet][column_name]:
+                    cell.alignment = Alignment(horizontal='left',wrap_text=True)
+            else:
+                wb[name_sheet].column_dimensions[column_name].width = adjusted_width+2
+        count_index += 1
+
+    return wb
